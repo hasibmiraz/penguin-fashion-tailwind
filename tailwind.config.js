bfa7e1d3d45365574bfe98e40647ea94684a390f
hasibmiraz <hasibmiraz09@gmail.com>
2022-01-23 01:03:23 +0600

    Header Section Complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        penguin: '#FFFBF0',
+      },
+    },
   },
   plugins: [],
 };
